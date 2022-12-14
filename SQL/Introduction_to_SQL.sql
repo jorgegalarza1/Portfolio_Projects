0db627ca98b46422ddc277add5ef9e417e00ae3d
Jorge Galarza <107782717+jorgegalarza1@users.noreply.github.com>
2022-12-14 04:13:35 -0700

    Update Introduction_to_SQL.sql

--- SQL/Introduction_to_SQL.sql
+++ SQL/Introduction_to_SQL.sql
@@ -3,10 +3,10 @@
 -- Columns = fields
 -- Columns are set when the dataset was created
 
--- Select all fields
+-- Select all fields (columns)
 SELECT *
--- From the 'books' table
-FROM books
+-- From the 'sample_table' table
+FROM sample_table
 
 -- VARCHAR is a flexible and popular string data type in SQL
 -- INT is a flexible and popular integer data type in SQL
@@ -17,48 +17,51 @@ FROM books
 
 -- SELECT: Select which fields should be selected
 -- FROM: Indicates the table in which the fields are located
-SELECT name
-FROM patrons;
+SELECT column_1
+FROM sample_table;
 
 -- Select multiple fields (columns)
-SELECT card_num, name
-FROM patrons;
+SELECT column_1, column_2
+FROM sample_table;
 
 -- Select three fields
-SELECT name, card_num, total_fine
-FROM patrons;
+SELECT column_1, column_2, column_3
+FROM sample_table;
 
--- Select all fields from the "patrons" table
+-- Select all fields from the "sample_table" table
 SELECT *
-FROM patrons;
+FROM sample_table;
 
--- Select name and year_hired from employees table, rename 'name' to 'first_name'
-SELECT name AS first_name, year_hired
-FROM employees;
+-- Select column_1 and column_2 from "sample_table" table, rename 'column_1' to 'first_mod'
+SELECT column_1 AS first_mod, column_2
+FROM sample_table;
 
--- Select distinct records from the field year_hired (no duplicates)
-SELECT DISTINCT year_hired
-FROM employees;
+-- Select distinct records from the field column_year (no duplicates)
+SELECT DISTINCT column_year
+FROM sample_table;
 
 -- All unique, no duplicate entries
-SELECT DISTINCT dept_id, year_hired
-FROM employees;
+SELECT DISTINCT column_1, column_year
+FROM sample_table;
 
 -- Saving SQL resulting from the SELECT statement
--- Create a view called employees_hire_years
-CREATE VIEW employees_hire_years AS
-SELECT id, name, year_hired
-FROM employees
+-- Create a view (table) called new_table
+CREATE VIEW new_table AS
+SELECT column_1, column_2, column_3
+FROM sample_table;
 
-SELECT id, name
-FROM employees_hire_years
+-- Now you can also work with your new table
+SELECT column_1, column_2
+FROM new_table;
 
 -- PostgreSQL (free and open source)
-SELECT id, name
-FROM employees
+-- From sample_table, get column_1 and column_2, and only retain the top 2 records (rows)
+SELECT column_1, column_2
+FROM sample_table
 LIMIT 2;
 
 -- SQL Server (Created by Microsoft, has both a free and a enterprise version, is queried using T-SQL)
-SELECT id, name
-FROM employees
+-- From sample_table, get column_1 and column_2, and only retain the top 2 records (rows)
+SELECT column_1, column_2
+FROM sample_table
 TOP 2;
